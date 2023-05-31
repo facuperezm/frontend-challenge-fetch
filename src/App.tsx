c6ce6ea4af2737fa3fa897f172d12c5dec231669
Facundo Perez Montalvo <facundoperezmontalvo@gmail.com>
2023-05-31 10:22:45 -0300

    checkpoint

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,50 @@ const URL = `https://randomuser.me/api/?results=100`
 
 function App() {
 	const [users, setUsers] = React.useState<User[]>([])
+	const [showColors, setShowColors] = React.useState(false)
+	const [sortByCountry, setSortByCountry] = React.useState(false)
 
 	React.useEffect(() => {
 		fetch(URL)
 			.then(res => res.json())
-			.then(data => setUsers(data.results))
+			.then(data => {
+				setUsers(data.results)
+				setSortByCountry(data.results)
+			})
 			.catch(err => console.error(err))
 	}, [])
+
+	const toggleSortByCountry = () => {
+		setSortByCountry(!sortByCountry)
+	}
+
+	const sortedUsers = sortByCountry
+		? [...users].sort((a, b) =>
+				a.location.country.localeCompare(b.location.country)
+		  )
+		: users
+
+	const handleDelete = (email: string) => {
+		const newUsers = users.filter(user => user.email !== email)
+		setUsers(newUsers)
+	}
 
 	return (
 		<>
 			<h1>Frontend Challenge</h1>
-			<UserList users={users} />
+			<header>
+				<button onClick={() => setShowColors(!showColors)}>
+					Cambiar color
+				</button>
+				<button onClick={toggleSortByCountry}>Ordenar por pais</button>
+			</header>
+			<main>
+				<UserList
+					users={sortedUsers}
+					showColors={showColors}
+					handleDelete={handleDelete}
+				/>
+			</main>
 		</>
 	)
 }
--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,32 +1,42 @@
 import { User } from '../types'
 
-export function UserList({ users }: { users: User[] }) {
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>Picture</th>
-          <th>Name</th>
-          <th>Last Name</th>
-          <th>Country</th>
-          <th>Action</th>
-        </tr>
-      </thead>
-      <tbody>
-        {users.map(user => {
-          return (
-            <tr key={user.email}>
-              <td>
-                <img src={user.picture.thumbnail} alt='user' />
-              </td>
-              <td>{user.name.first}</td>
-              <td>{user.name.last}</td>
-              <td>{user.location.country}</td>
-              <td><button>Delete</button></td>
-            </tr>
-          )
-        })}
-      </tbody>
-    </table>
-  )
+interface Props {
+	users: User[]
+	showColors: boolean
+	handleDelete: (email: string) => void
+}
+
+export function UserList({ users, showColors, handleDelete }: Props) {
+	return (
+		<table width='100%'>
+			<thead>
+				<tr>
+					<th>Picture</th>
+					<th>Name</th>
+					<th>Last Name</th>
+					<th>Country</th>
+					<th>Action</th>
+				</tr>
+			</thead>
+			<tbody>
+				{users.map((user, index) => {
+					const color = index % 2 === 0 ? '#333' : '#555'
+					const bgcolor = showColors ? color : 'transparent'
+					return (
+						<tr key={user.email} style={{ backgroundColor: `${bgcolor}` }}>
+							<td>
+								<img src={user.picture.thumbnail} alt='user' />
+							</td>
+							<td>{user.name.first}</td>
+							<td>{user.name.last}</td>
+							<td>{user.location.country}</td>
+							<td>
+								<button onClick={handleDelete}>Delete</button>
+							</td>
+						</tr>
+					)
+				})}
+			</tbody>
+		</table>
+	)
 }
