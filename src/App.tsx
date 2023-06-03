08cb93ee755709cfd6ab4598e5448b1012731207
Facundo Perez Montalvo <facundoperezmontalvo@gmail.com>
2023-06-03 10:20:47 -0300

    feat: :zap: Added React Query to the project for async states
    This has been done to improve performance and for learning purposes

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,35 @@ import './App.css'
 import { type User } from './types.d'
 import { SortBy } from './types.d'
 import { UserList } from './components/UserList'
+import { useQuery } from 'react-query'
 
-const URL = `https://randomuser.me/api/?results=100`
+const fetchUsers = async (page: number) => {
+	return await fetch(
+		`https://randomuser.me/api?results=10&seed=abc&page=${page}`
+	)
+		.then(async res => {
+			if (!res.ok)
+				throw new Error(`Error fetching users: ${res.status} ${res.statusText}`)
+			return await res.json()
+		})
+		.then(data => data.results)
+		.catch(err => console.error(err))
+}
 
 function App() {
-	const [users, setUsers] = React.useState<User[]>([])
+	// const [users, setUsers] = React.useState<User[]>([])
 	const [showColors, setShowColors] = React.useState(false)
 	const [sortByCountry, setSortByCountry] = React.useState(false)
 	const [filter, setFilter] = React.useState<string | null>(null)
 	const [sorting, setSorting] = React.useState<SortBy>(SortBy.NONE)
+	const [currentPage, setCurrentPage] = React.useState(1)
 	const originalUser = React.useRef<User[]>([])
 
-	React.useEffect(() => {
-		fetch(URL)
-			.then(res => res.json())
-			.then(data => {
-				setUsers(data.results)
-				setSortByCountry(data.results)
-				originalUser.current = data.results
-			})
-			.catch(err => console.error(err))
-	}, [])
+	const {
+		isLoading,
+		isError,
+		data: users = []
+	} = useQuery<User[]>('users', async () => fetchUsers(currentPage))
 
 	const toggleSortByCountry = () => {
 		setSortByCountry(!sortByCountry)
@@ -79,12 +87,32 @@ function App() {
 				/>
 			</header>
 			<main>
-				<UserList
-					changeSorting={setSorting}
-					users={sortedUsers}
-					showColors={showColors}
-					handleDelete={handleDelete}
-				/>
+				{users.length > 0 && (
+					<UserList
+						changeSorting={setSorting}
+						users={sortedUsers}
+						showColors={showColors}
+						handleDelete={handleDelete}
+					/>
+				)}
+
+				{isLoading && <strong>Loading...</strong>}
+
+				{isError && <strong>Error!</strong>}
+
+				{!isLoading && !isError && users.length === 0 && (
+					<strong>No users found</strong>
+				)}
+
+				{/* {!isLoading && !isError && users.length > 0 && (
+					<strong>Showing {users.length} users</strong>
+				)} */}
+
+				{!isLoading && !isError && users.length > 0 && (
+					<button onClick={() => setCurrentPage(currentPage + 1)}>
+						Cargar más resultados
+					</button>
+				)}
 			</main>
 		</>
 	)
